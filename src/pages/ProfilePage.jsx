a26443b2369fa26758f0e4616d7c2ee99f4ff9d0
Alexandr Sudakov <sudakovalexandrwork@gmail.com>
2026-06-05 20:56:52 +0300

    Create ProfilePage.jsx

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+    return (
+        <div className="page">
+            <h2>Profile</h2>
+            <p>This app works offline and stores data on your device.</p>
+        </div>
+    );
+}
